f9f0018c331cff30ecf4445621b7e2e0c8e42802
Shravan Shetty <shravanshetty322@gmail.com>
2022-04-08 22:25:50 +0530

    added todos

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,9 @@ use std::time::Duration;
 const JUPITER_PROGRAM: &str = "JUP2jxvXaqu7NQY1GmNF4m1vodw12LVXYxbFL2uJvfo";
 const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 
-// TODO get all jupiter transactions which are stable swaps
+// TODO refactor
+// TODO fix bugs
+// TODO store in DB
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rpc_url = "https://api.mainnet-beta.solana.com".to_string();
     let ws_url = "wss://api.mainnet-beta.solana.com".to_string();
