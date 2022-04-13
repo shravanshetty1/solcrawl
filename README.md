73966dedc0693b7f1db3a98e5b13bde04a9cc32e
Shravan Shetty <shravanshetty322@gmail.com>
2022-04-13 06:33:59 +0530

    crawler not automatically creates tables if it doesnt exist

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 
 ```bash
 cd scripts/crawler
-cargo run
+DATABASE_URL=postgres://postgres:test123@localhost/anon cargo run
 ```
+Replace the database url for your database instance. 
+Tables will be created automatically by the program if it doesn't exist.
--- scripts/crawler/src/main.rs
+++ scripts/crawler/src/main.rs
@@ -5,6 +5,9 @@ use std::error::Error;
 #[macro_use]
 extern crate diesel;
 
+#[macro_use]
+extern crate diesel_migrations;
+
 pub mod handle_txs;
 pub mod storage;
 
@@ -17,6 +20,8 @@ const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 const USDC_MINT: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 const USDT_MINT: &str = "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB";
 const UST_MINT: &str = "9vMJfxuKxXBoEa7rM12mYLMwTacLMLDJqHozw96WQL8i";
+
+diesel_migrations::embed_migrations!();
 
 // crawling jupiter for stable swaps
 fn main() -> Result<(), Box<dyn Error>> {
@@ -38,6 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     let conn = storage::conn::establish_connection()?;
+    embedded_migrations::run(&conn)?;
     std::thread::spawn(move || crate::handle_txs::handle_txs(conn, recv));
 
     println!("started crawling, please wait - establishing web socket connection (this can take upto 20 seconds)");
